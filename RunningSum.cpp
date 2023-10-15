fd3b21ac50ea198c12ca7c4eb71636568225f271
sahilyadav-1 <121487105+sahilyadav-1@users.noreply.github.com>
2023-10-16 00:09:46 +0530

    Create RunningSum.cpp

---
+++ RunningSum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> ans;
+        int res = 0;
+        for(int i = 0; i < nums.size(); i++){
+            res += nums[i];
+            ans.push_back(res);
+        }
+        return ans;
+    }
+};
